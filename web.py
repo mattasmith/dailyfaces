4d0fd585ae548e01d09d67bc1354db0f6b23aa23
mattasmith <matt@Matt-Smiths-MacBook-Pro.local>
2014-01-28 20:02:36 -0800

    changed_port

--- web.py
+++ web.py
@@ -47,4 +47,4 @@ def regularpage(pagename=None):
 	return "You've arrived at " + pagename
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=8080, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
