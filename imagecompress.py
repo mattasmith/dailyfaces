c2effc3636f559bf4ee475c526749d211b425c39
mattasmith <matt@Matt-Smiths-MacBook-Pro.local>
2014-01-30 20:06:01 -0800

    compress first 10 images handle missing images

--- imagecompress.py
+++ imagecompress.py
@@ -5,26 +5,26 @@ from PIL import Image
 
 def resizeImage(image_file):
 	# get the image's width and height in pixels
-	img = Image.open(image_file)
-	width, height = img.size
+	with Image.open(image_file) as img:
+		width, height = img.size
 
-	# get the largest dimension
-	max_dim = max(img.size)
+		# get the largest dimension
+		max_dim = max(img.size)
 
-	if max_dim > 1000:
-	# resize the image using the largest side as dimension
-		factor = 1000./max_dim
-		new_width = int(width*factor)
-		new_height = int(height*factor)
-		resized_image = img.resize((new_width, new_height), Image.ANTIALIAS)
-		print width, height, new_width, new_height
+		if max_dim > 1000:
+		# resize the image using the largest side as dimension
+			factor = 1000./max_dim
+			new_width = int(width*factor)
+			new_height = int(height*factor)
+			resized_image = img.resize((new_width, new_height), Image.ANTIALIAS)
+			print width, height, new_width, new_height
 
-		# save the resized image to a file
-		# overwrite existing file
-		resized_image_file = image_file
-		resized_image.save(resized_image_file)
-		#
-		print("%s resized" % resized_image_file)
+			# save the resized image to a file
+			# overwrite existing file
+			resized_image_file = image_file
+			resized_image.save(resized_image_file)
+			#
+			print("%s resized" % resized_image_file)
 	
 
 # pick an image file you have in the working directory
