8babd55dbc550d3cad1f92ecec1e489f797c512b
mattasmith <matt@unknowne4ce8f345762.gateway.2wire.net>
2014-02-02 19:11:55 -0800

    catch many more unusual characters when decoding utf8, remove threading

--- pullrss4.py
+++ pullrss4.py
@@ -17,7 +17,15 @@ def unencode_text(text):
 
 def decode_text(text):
 	try:
-		cleantext = text.decode('utf8','ignore')
+		# remove odd characters, then decode
+		normtext = text.replace(u'\u2019','\'').replace(u'\u2018','\'')
+		normtext = normtext.replace(u'\xbd','').replace(u'\xa0','')
+		normtext = normtext.replace(u'\u201c','\"').replace(u'\u201d','\"')
+		normtext = normtext.replace(u'\xe9','e').replace(u'\xe0','a').replace(u'\xe8','e').replace(u'\xf8','o').replace(u'\xe1','a')
+		normtext = normtext.replace(u'\u2014', '-').replace(u'\u2013', '-')
+		normtext = normtext.replace(u'\u2026', '...')
+		normtext = normtext.replace(u'\u2009', '')
+		cleantext = normtext.decode('utf8','ignore')
 	except:
 		print 'Error with decoding %s' % text
 		cleantext = ''
@@ -63,9 +71,11 @@ with open('hitlist.txt', 'r') as f:
 	hit_list = [hit for hit in f.read().split('\n') if hit] # list of feeds to pull down
  
 # pull down all feeds
-future_calls = [Future(feedparser.parse,rss_url) for rss_url in hit_list]
+#future_calls = [Future(feedparser.parse,rss_url) for rss_url in hit_list]
 # block until they are all in
-feeds = [future_obj() for future_obj in future_calls]
+#feeds = [future_obj() for future_obj in future_calls]
+feeds = [feedparser.parse(rss_url) for rss_url in hit_list]
+
 
 entries = []
 for feed in feeds:
@@ -78,23 +88,23 @@ html_parser = HTMLParser.HTMLParser()
 con = mdb.connect('localhost', 'testuser', 'test123', 'rssfeeddata', charset='utf8')
 with con:
 	cur = con.cursor(mdb.cursors.DictCursor)
-	# in entries, I want link and summary and title and published.
-	# need to parse summary to text
+ 	# in entries, I want link and summary and title and published.
+ 	# need to parse summary to text
 	for entry in entries:
 		title = entry["title"]
 		#title = title.encode("ascii", "ignore")
 		#title = html_parser.unescape(title)
-		# ignore strange non-html, non-unicode characters
+ 		# ignore strange non-html, non-unicode characters
 		title = unencode_text(title)
-		if 'published_parsed' in entry and entry["published_parsed"] is not None:
-			datepublished = extract_pubdate(entry["published_parsed"])
-		link = entry["link"]
+ 		if 'published_parsed' in entry and entry["published_parsed"] is not None:
+ 			datepublished = extract_pubdate(entry["published_parsed"])
+ 		link = entry["link"]
 		summarytext = extract_summarytext(entry)
 		title = decode_text(title)
 		summarytext = decode_text(summarytext)
-		# CREATE TABLE news_exp (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, entrydate DATE NOT NULL, pubdate DATE NOT NULL, title TEXT NOT NULL, summary TEXT NOT NULL, link TEXT NOT NULL);
-		sql = "INSERT INTO news3 VALUES (NULL, CURDATE(), %s, %s, %s, %s)"
-		cur.execute(sql, (datepublished, title, summarytext, link) )
+ 		# CREATE TABLE news_exp (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, entrydate DATE NOT NULL, pubdate DATE NOT NULL, title TEXT NOT NULL, summary TEXT NOT NULL, link TEXT NOT NULL);
+ 		sql = "INSERT INTO news3 VALUES (NULL, CURDATE(), %s, %s, %s, %s)"
+ 		cur.execute(sql, (datepublished, title, summarytext, link) )
 
 
 
