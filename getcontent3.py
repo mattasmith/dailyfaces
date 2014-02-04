81ccdc37045ac0f703417b54bbcdfd84c7e51977
mattasmith <matt@Matt-Smiths-MacBook-Pro.local>
2014-02-04 09:59:52 -0800

    updated date

--- getcontent3.py
+++ getcontent3.py
@@ -148,7 +148,7 @@ with con:
 	# get links for each unique article
 	cur.execute("SELECT id, link \
 		FROM article3 \
-		WHERE entrydate='2014-02-03'; ") # get all links to news
+		WHERE entrydate='2014-02-04'; ") # get all links to news
 	rows = cur.fetchall()
 
 
