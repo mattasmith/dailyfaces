85864ccbc5742505377c9d832412aaaa85a07eec
mattasmith <matt@Matt-Smiths-MacBook-Pro.local>
2014-02-04 16:46:56 -0800

    fix bug when searching for person, search for id instead

--- imagedownload.py
+++ imagedownload.py
@@ -55,8 +55,8 @@ for row in rows:
 		cur = con.cursor(mdb.cursors.DictCursor)
         person = row['person']
         imageurl = image_search(person)
-        sql = "UPDATE people3 SET imageurl=%s WHERE person=%s"
-        cur.execute(sql, (imageurl, person) )
+        sql = "UPDATE people3 SET imageurl=%s WHERE id=%s"
+        cur.execute(sql, (imageurl, row['id']) )
         print row['id']
         try:
             urllib.urlretrieve(imageurl, "static/images/download/%s.jpg" %  str(row['id']) )
