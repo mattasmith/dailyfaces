4e252307e4a83ef0bb9d19c58b504d527516064a
mattasmith <matt@Matt-Smiths-MacBook-Pro.local>
2014-02-04 16:51:38 -0800

    fix bug when searching for url, replace unicode if imageurl exists

--- imagedownload.py
+++ imagedownload.py
@@ -55,7 +55,8 @@ for row in rows:
 		cur = con.cursor(mdb.cursors.DictCursor)
         person = row['person']
         imageurl = image_search(person)
-        imageurl.replace(u'\u2019',u'\'')
+        if imageurl:
+            imageurl.replace(u'\u2019',u'\'')
         sql = "UPDATE people3 SET imageurl=%s WHERE id=%s"
         cur.execute(sql, (imageurl, row['id']) )
         print row['id']
