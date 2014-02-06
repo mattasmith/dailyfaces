e097aebb1ae009b9f56bb0bea718be7b75996368
mattasmith <matt@Matt-Smiths-MacBook-Pro.local>
2014-02-06 14:34:04 -0800

    updated date

--- web.py
+++ web.py
@@ -10,12 +10,12 @@ con = mdb.connect('localhost', 'testuser', 'test123', 'rssfeeddata')
 @app.route("/")
 @app.route("/index.html")
 def hello():
-	toppeople = sqlqueries.peopleinthenews('2014-02-04')
+	toppeople = sqlqueries.peopleinthenews('2014-02-06')
 	return render_template('index.html', toppeople=toppeople)
 
 @app.route("/db")
 def top():
-	toppeople = sqlqueries.peopleinthenews('2014-02-04')
+	toppeople = sqlqueries.peopleinthenews('2014-02-06')
 	return toppeople[2].tagid
 
 
@@ -23,7 +23,7 @@ def top():
 def search():
     keyword = request.args.get('q', None)
 
-    toppeople = sqlqueries.peopleinthenews('2014-02-04', keyword)   
+    toppeople = sqlqueries.peopleinthenews('2014-02-06', keyword)   
 
     return render_template('search.html',
             toppeople=toppeople,
